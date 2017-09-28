2d15c49b023787ca563f8476160eb35e3f7fb00c
Boomaa23 <cmo93003@yahoo.com>
2017-09-28 16:32:01 -0700

    Changes contact form numbering system

--- supportscript.php
+++ supportscript.php
@@ -1,16 +1,18 @@
 <?php
+function generateRandomString($length = 10) {
+    return substr(str_shuffle(str_repeat($x='0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+}
+
 if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tt']) && isset($_POST['t_main'])) {
-	$file = rand(0000,9999);
+	//$file = rand(0000,9999); 
+	$file = generateRandomString();
 	$usr = "Username: ";
 	$email = "Email Address: ";
 	$ttype = "Ticket Type: ";
 	$num = "Ticket Number: ";
+	
     $data = $num . $file . "\n" . $usr . $_POST['name'] . "\n" . $email . $_POST['email'] . "\n" . $ttype . $_POST['tt'] . "\n" . "\n" . $_POST['t_main'] . "\n";
-    
-    // use double tics so that $file is inserted.
-    // like this: "/messages/$file.txt"
-    // alternatively, we could use: '/messages/' . $file . '.txt'
-    // (for those who are married to single tics)
+
     $ret = file_put_contents("support_tickets/$file.txt", $data , FILE_APPEND | LOCK_EX);
     if($ret === false) {
         die('There was an error writing this file');
@@ -22,5 +24,4 @@ if(isset($_POST['name']) && isset($_POST['email']) && isset($_POST['tt']) && iss
 else {
    die('no post data to process');
 }
-
 ?>
